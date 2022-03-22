375f592521a3135a3546d9bbceeca2fd20d26c53
Ryan Blackmore <3052414+rblackmore@users.noreply.github.com>
2022-03-23 09:27:42 +1100

    SimpleConsoleApp Now has optionsal cancellationToken parameter.

--- src/IGE.SimpleConsole/SimpleConsoleApp.cs
+++ src/IGE.SimpleConsole/SimpleConsoleApp.cs
@@ -14,7 +14,9 @@ public abstract class SimpleConsoleApp : IAsyncSimpleComponent
 
   private bool isExited = false;
 
-  public SimpleConsoleApp(ScreenManager screenManager, SimpleConsoleAppOptions options = null!)
+  public SimpleConsoleApp(
+    ScreenManager screenManager,
+    SimpleConsoleAppOptions options = null!)
   {
     this.screenManager = Guard.Against.Null(screenManager, nameof(screenManager));
 
@@ -23,21 +25,21 @@ public abstract class SimpleConsoleApp : IAsyncSimpleComponent
 
   protected ScreenManager ScreenManager => this.screenManager;
 
-  public virtual async Task InitializeAsync(CancellationToken token)
+  public virtual async Task InitializeAsync(CancellationToken token = default)
   {
     await this.screenManager.InitializeAsync(token);
   }
 
-  public virtual async Task PrintAsync(CancellationToken token)
+  public virtual async Task PrintAsync(CancellationToken token = default)
   {
     await this.screenManager.PrintAsync(token);
   }
 
-  public async Task RunAsync(CancellationToken token)
+  public async Task RunAsync(CancellationToken token = default)
   {
     await this.InitializeAsync(token);
 
-    while (!this.isExited)
+    while (!token.IsCancellationRequested)
     {
       AnsiConsole.Clear();
       await this.PrintAsync(token);
